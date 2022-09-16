29c68793ab1fc1cf82126d18b1aa0f460438b149
André Guimarães de Barros <55102776+andregbarros64@users.noreply.github.com>
2022-09-16 06:44:03 -0300

    Inclusão do scrollbar que não estava funcionando porque precisava do grid layout

--- Pesquisa Eleitoral/src/view/TotalizacaoUI.java
+++ Pesquisa Eleitoral/src/view/TotalizacaoUI.java
@@ -17,6 +17,10 @@ import view.tables.ConsultaPesquisaTableModel;
 
 import javax.swing.JTextField;
 import javax.swing.JLabel;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JScrollBar;
 
 public class TotalizacaoUI extends JInternalFrame {
 	private JTable jTotalizacao;
@@ -42,9 +46,13 @@ public class TotalizacaoUI extends JInternalFrame {
 	 */
 	public TotalizacaoUI() {
 		setTitle("Totalização da pesquisa");
-		setBounds(100, 100, 596, 429);
-		
-		JButton bttotalizar = new JButton("Totalizar");
+		setBounds(100, 100, 546, 393);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{106, 271, 115, 0, 0};
+		gridBagLayout.rowHeights = new int[]{177, 110, 32, 29, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		getContentPane().setLayout(gridBagLayout);
 		
 		JButton btcancelar = new JButton("Fechar");
 		btcancelar.addActionListener(new ActionListener() {
@@ -55,35 +63,36 @@ public class TotalizacaoUI extends JInternalFrame {
 		
 		jTotalizacao = new JTable();
 		jTotalizacao.setModel(new ConsultaPesquisaTableModel(new PesquisasQuestionariosController().listar()));
-		//scrollPane.setViewportView(jTotalizacao);
-		//getContentPane().setLayout(groupLayout);
+		GridBagConstraints gbc_jTotalizacao = new GridBagConstraints();
+		gbc_jTotalizacao.gridheight = 2;
+		gbc_jTotalizacao.fill = GridBagConstraints.BOTH;
+		gbc_jTotalizacao.insets = new Insets(0, 0, 5, 5);
+		gbc_jTotalizacao.gridwidth = 3;
+		gbc_jTotalizacao.gridx = 0;
+		gbc_jTotalizacao.gridy = 0;
+		getContentPane().add(jTotalizacao, gbc_jTotalizacao);
 		
-		GroupLayout groupLayout = new GroupLayout(getContentPane());
-		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.TRAILING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(279)
-					.addComponent(bttotalizar, GroupLayout.PREFERRED_SIZE, 98, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(btcancelar, GroupLayout.PREFERRED_SIZE, 98, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(91, Short.MAX_VALUE))
-				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap(87, Short.MAX_VALUE)
-					.addComponent(jTotalizacao, GroupLayout.PREFERRED_SIZE, 411, GroupLayout.PREFERRED_SIZE)
-					.addGap(74))
-		);
-		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(46)
-					.addComponent(jTotalizacao, GroupLayout.PREFERRED_SIZE, 241, GroupLayout.PREFERRED_SIZE)
-					.addGap(32)
-					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(btcancelar)
-						.addComponent(bttotalizar))
-					.addContainerGap(35, Short.MAX_VALUE))
-		);
-		getContentPane().setLayout(groupLayout);
+		JScrollBar scrollBar = new JScrollBar();
+		GridBagConstraints gbc_scrollBar = new GridBagConstraints();
+		gbc_scrollBar.gridheight = 2;
+		gbc_scrollBar.insets = new Insets(0, 0, 5, 0);
+		gbc_scrollBar.gridx = 3;
+		gbc_scrollBar.gridy = 0;
+		getContentPane().add(scrollBar, gbc_scrollBar);
+		
+		JButton bttotalizar = new JButton("Totalizar");
+		GridBagConstraints gbc_bttotalizar = new GridBagConstraints();
+		gbc_bttotalizar.anchor = GridBagConstraints.NORTHEAST;
+		gbc_bttotalizar.insets = new Insets(0, 0, 0, 5);
+		gbc_bttotalizar.gridx = 1;
+		gbc_bttotalizar.gridy = 3;
+		getContentPane().add(bttotalizar, gbc_bttotalizar);
+		GridBagConstraints gbc_btcancelar = new GridBagConstraints();
+		gbc_btcancelar.insets = new Insets(0, 0, 0, 5);
+		gbc_btcancelar.anchor = GridBagConstraints.NORTHWEST;
+		gbc_btcancelar.gridx = 2;
+		gbc_btcancelar.gridy = 3;
+		getContentPane().add(btcancelar, gbc_btcancelar);
 
 	}
 
@@ -92,5 +101,4 @@ public class TotalizacaoUI extends JInternalFrame {
 			  javax.swing.JPanel __wbp_panel = new javax.swing.JPanel();
 		}
 	}
-
 }
